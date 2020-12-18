c4a6149cf4ba77d413a7fc8f92be3f4a7d647bec
VishalBeetonz <53287764+VishalBeetonz@users.noreply.github.com>
2020-12-18 11:40:38 +0530

    Create fencinglefttoright.glsl

---
+++ transitions/fencinglefttoright.glsl
@@ -0,0 +1,18 @@
+// Author: Vishal
+// License: MIT
+
+uniform float count; // = 10.0
+vec2 direction = vec2(0.0,0.0); // = vec2(0.0, 0.0)
+
+
+vec4 transition (vec2 uv) {
+
+  vec2 vv = vec2(1.0,1.0);
+    vv.y = (0.2 * progress)+(1.8);
+  vec2 p = uv + vv-(progress/10.0) * sign(direction);
+  vec2 f = fract(p);
+
+    float pr = progress*1.2;
+    float s = step(pr, fract(count * uv.x));
+    return mix(getToColor(f),getFromColor(uv), s);
+}
